88cf6a1bee72a1f157cbf451811285a24e8e3597
Son0-0 <jsmith032211@gmail.com>
2022-05-11 13:10:12 +0000

    study

---
+++ JG STUDY/alps/7-two-sum.py
@@ -0,0 +1,9 @@
+# 3867 ms 14.9 MB
+# https://leetcode.com/problems/two-sum/
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
