c5be5686dff5d60bf6008526fd3ba7ebb7af1eef
Son0-0 <jsmith032211@gmail.com>
2022-05-30 13:10:12 +0000

    study

---
+++ JG STUDY/alps/30-longest-substring-without-repeating-characters.py
@@ -0,0 +1,24 @@
+# https://leetcode.com/problems/longest-substring-without-repeating-characters
+from collections import deque
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if len(s) < 2:
+            return len(s)
+        
+        substring = deque()
+        max_len = 0
+
+        for c in s:
+
+            if c in substring:
+                while substring:
+                    cur = substring.popleft()
+                    if cur == c:
+                      break
+
+            if c not in substring:
+                substring.append(c)
+                max_len = max(max_len, len(substring))
+
+        return max_len
