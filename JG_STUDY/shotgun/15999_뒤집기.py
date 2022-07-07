48c39955d1454d6788c9ace1c8c84811f9231d2a
Son0-0 <jsmith032211@gmail.com>
2022-07-07 23:57:27 +0900

    sg: 15999_뒤집기.py

---
+++ JG_STUDY/shotgun/15999_뒤집기.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+_map = []
+for _ in range(n):
+  _map.append(list(map(str, input().strip())))
+
+def solution():
+  
+  
+  
+  return 0
+  
+solution()
