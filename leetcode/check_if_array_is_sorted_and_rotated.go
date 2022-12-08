e2e8f31b50a91ee50374d9c78f9c3beb55dae03f
Son0-0 <jsmith032211@gmail.com>
2022-12-08 23:52:05 +0900

    solve: leetcode 22/12/08

---
+++ leetcode/check_if_array_is_sorted_and_rotated.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func check(nums []int) bool {
+	result := 0
+	length := len(nums)
+	
+	for i := 0; i < length; i++ {
+		if nums[(i + 1) % length] < nums[i] {
+			result += 1
+		}
+	}
+
+	return result <= 1
+}
+
+func main() {
+	fmt.Println(check([]int{3, 4, 5, 1, 2}))
+	fmt.Println(check([]int{2, 1, 3, 4}))
+	fmt.Println(check([]int{1, 2, 3}))
+}
