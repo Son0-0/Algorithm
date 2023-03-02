d2a3b184bec5c4f05db8346480c0b447db67f165
Seungwoo Son <jsmith032211@gmail.com>
2023-03-02 23:08:17 +0900

    solve: leetcode 23/03/02

---
+++ leetcode/easy/binary-tree-preorder-traversal.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func preorderTraversal(root *TreeNode) []int {
+    result := []int{}
+
+    var preorder func(*TreeNode)
+
+    preorder = func(cur *TreeNode) {
+        if cur == nil {
+            return
+        }
+
+        result = append(result, cur.Val)
+        preorder(cur.Left)
+        preorder(cur.Right)
+    }
+
+    preorder(root)
+
+    return result
+}
