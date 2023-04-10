cde74268ca81d00609d2fbecbb91eb8e32969b2c
Son0-0 <jsmith032211@gmail.com>
2023-04-10 23:04:49 +0900

    solve: leetcode 23/04/10

--- leetcode/easy/valid-parentheses.go
+++ leetcode/easy/valid-parentheses.go
@@ -2,44 +2,75 @@ package main
 
 import "fmt"
 
+// func isValid(s string) bool {
+// 	stack := []rune{}
+
+// 	for _, c := range s {
+// 		if c == '(' || c == '{' || c == '[' {
+// 			stack = append(stack, c)
+// 		} else {
+// 			if len(stack) == 0 {
+// 				return false
+// 			}
+
+// 			if c == ')' {
+// 				if stack[len(stack)-1] == '(' {
+// 					stack = stack[:len(stack)-1]
+// 				} else {
+// 					return false
+// 				}
+// 			} else if c == '}' {
+// 				if stack[len(stack)-1] == '{' {
+// 					stack = stack[:len(stack)-1]
+// 				} else {
+// 					return false
+// 				}
+// 			} else {
+// 				if stack[len(stack)-1] == '[' {
+// 					stack = stack[:len(stack)-1]
+// 				} else {
+// 					return false
+// 				}
+// 			}
+// 		}
+// 	}
+
+// 	return len(stack) == 0
+// }
+
 func isValid(s string) bool {
-	stack := []rune{}
+	stack := make([]rune, len(s))
+	var top int
 
 	for _, c := range s {
-		if c == '(' || c == '{' || c == '[' {
-			stack = append(stack, c)
-		} else {
-			if len(stack) == 0 {
+		switch c {
+		case '(', '{', '[':
+			stack[top] = c
+			top++
+		case ')':
+			if top == 0 || stack[top-1] != '(' {
 				return false
 			}
-
-			if c == ')' {
-				if stack[len(stack)-1] == '(' {
-					stack = stack[:len(stack)-1]
-				} else {
-					return false
-				}
-			} else if c == '}' {
-				if stack[len(stack)-1] == '{' {
-					stack = stack[:len(stack)-1]
-				} else {
-					return false
-				}
-			} else {
-				if stack[len(stack)-1] == '[' {
-					stack = stack[:len(stack)-1]
-				} else {
-					return false
-				}
+			top--
+		case '}':
+			if top == 0 || stack[top-1] != '{' {
+				return false
 			}
+			top--
+		case ']':
+			if top == 0 || stack[top-1] != '[' {
+				return false
+			}
+			top--
 		}
 	}
 
-	return len(stack) == 0
+	return top == 0
 }
 
 func main() {
 	fmt.Println(isValid("()"))
 	fmt.Println(isValid("()[]{}"))
 	fmt.Println(isValid("(]"))
+	fmt.Println(isValid("["))
 }
