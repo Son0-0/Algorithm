46397bd6dd36518338b38c2a6e29bb6079997921
Son0-0 <jsmith032211@gmail.com>
2023-05-07 23:58:49 +0900

    solve: leetcode 23/05/07

--- leetcode/easy/number-of-good-pairs.go
+++ leetcode/easy/number-of-good-pairs.go
@@ -3,7 +3,25 @@ package main
 import "fmt"
 
 func numIdenticalPairs(nums []int) int {
+	size := len(nums)
 	result := 0
+
+	var dfs func(int, int, []int)
+
+	dfs = func(cur, length int, visited []int) {
+		if length == 2 {
+			if nums[visited[0]] == nums[visited[1]] {
+				result++
+			}
+			return
+		}
+
+		for i := cur + 1; i < size; i++ {
+			dfs(i, length+1, append(visited, i))
+		}
+	}
+
+	dfs(-1, 0, []int{})
 
 	return result
 }
