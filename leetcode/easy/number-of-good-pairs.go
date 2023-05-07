3de18bc45068777ccbe433385f9775321e4841a2
Son0-0 <jsmith032211@gmail.com>
2023-05-07 23:54:52 +0900

    solving: leetcode 23/05/07

---
+++ leetcode/easy/number-of-good-pairs.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func numIdenticalPairs(nums []int) int {
+	result := 0
+
+	return result
+}
+
+func main() {
+	fmt.Println(numIdenticalPairs([]int{1, 2, 3, 1, 1, 3}))
+	fmt.Println(numIdenticalPairs([]int{1, 1, 1, 1}))
+	fmt.Println(numIdenticalPairs([]int{1, 2, 3}))
+}
