3fbda1e6ca756a151dd66709890c337f4cf53288
Son0-0 <jsmith032211@gmail.com>
2023-05-08 20:51:38 +0900

    solving: leetcode 23/05/08

---
+++ leetcode/easy/matrix-diagonal-sum.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func diagonalSum(mat [][]int) int {
+	result := 0
+
+	return result
+}
+
+func main() {
+	fmt.Println(diagonalSum([][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}))
+	fmt.Println(diagonalSum([][]int{{1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}}))
+}
