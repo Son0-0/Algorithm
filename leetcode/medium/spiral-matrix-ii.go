48a0055d2b2db153b23803963629145ea973d29e
Son0-0 <jsmith032211@gmail.com>
2023-05-10 23:53:47 +0900

    solve: leetcode 23/05/10

--- leetcode/medium/spiral-matrix-ii.go
+++ leetcode/medium/spiral-matrix-ii.go
@@ -2,8 +2,50 @@ package main
 
 import "fmt"
 
+type node struct {
+	x int
+	y int
+}
+
 func generateMatrix(n int) [][]int {
-	result := make([][]int, 3)
+	result := make([][]int, n)
+
+	for i := 0; i < n; i++ {
+		temp := make([]int, n)
+		result[i] = temp
+	}
+
+	dir := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
+	curDir := 0
+	cx, cy := 0, 0
+
+	visited := make(map[node]bool)
+	visited[node{x: cx, y: cy}] = true
+	result[cx][cy] = 1
+
+	cnt := 2
+
+	for cnt <= n*n {
+		nx, ny := cx+dir[curDir][0], cy+dir[curDir][1]
+
+		if (0 <= nx && nx < n) && (0 <= ny && ny < n) {
+			if _, e := visited[node{x: nx, y: ny}]; e {
+				curDir = (curDir + 1) % 4
+				nx, ny = cx+dir[curDir][0], cy+dir[curDir][1]
+			}
+		} else {
+			curDir = (curDir + 1) % 4
+			nx, ny = cx+dir[curDir][0], cy+dir[curDir][1]
+		}
+
+		cx, cy = nx, ny
+
+		visited[node{x: cx, y: cy}] = true
+
+		result[cx][cy] = cnt
+
+		cnt++
+	}
 
 	return result
 }
