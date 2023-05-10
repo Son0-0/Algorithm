e4be3632692ea78a219a2b4be3ab100ba02ea3d2
Son0-0 <jsmith032211@gmail.com>
2023-05-10 23:37:00 +0900

    solving: leetcode 23/05/10

---
+++ leetcode/medium/spiral-matrix-ii.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func generateMatrix(n int) [][]int {
+	result := make([][]int, 3)
+
+	return result
+}
+
+func main() {
+	fmt.Println(generateMatrix(3))
+	fmt.Println(generateMatrix(1))
+}
