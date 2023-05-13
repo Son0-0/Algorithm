8b04a901b546baeaceb4a622b64dba12bb267ec7
Son0-0 <jsmith032211@gmail.com>
2023-05-14 01:13:10 +0900

    solve: leetcode 23/05/13

--- leetcode/easy/toeplitz-matrix.go
+++ leetcode/easy/toeplitz-matrix.go
@@ -2,6 +2,22 @@ package main
 
 import "fmt"
 
+func isToeplitzMatrix(matrix [][]int) bool {
+	row := make([]int, len(matrix[0]))
+	copy(row, matrix[0])
+
+	for i := 1; i < len(matrix); i++ {
+		for j := 1; j < len(matrix[i]); j++ {
+			if matrix[i][j] != row[j-1] {
+				return false
+			}
+		}
+		copy(row, matrix[i])
+	}
+
+	return true
+}
+
 func main() {
-  fmt.Println("Hello Leetcode")
+	fmt.Println(isToeplitzMatrix([][]int{{1, 2, 3, 4}, {5, 1, 2, 3}, {9, 5, 1, 2}}))
 }
