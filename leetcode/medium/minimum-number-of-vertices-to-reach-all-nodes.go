0929625ddb25334d50b360d14179887224042e86
Son0-0 <jsmith032211@gmail.com>
2023-05-19 22:27:39 +0900

    solve: leetcode 23/05/18

--- leetcode/medium/minimum-number-of-vertices-to-reach-all-nodes.go
+++ leetcode/medium/minimum-number-of-vertices-to-reach-all-nodes.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func findSmallestSetOfVertices(n int, edges [][]int) []int {
+	result := make([]int, 0)
+	indegrees := make([]int, n)
+
+	for _, edge := range edges {
+		indegrees[edge[1]]++
+	}
+
+	for idx, v := range indegrees {
+		if v == 0 {
+			result = append(result, idx)
+		}
+	}
+
+	return result
+}
 
 func main() {
-  fmt.Println("Hello Leetcode")
+	fmt.Println(findSmallestSetOfVertices(6, [][]int{{0, 1}, {0, 2}, {2, 5}, {3, 4}, {4, 2}}))
 }
