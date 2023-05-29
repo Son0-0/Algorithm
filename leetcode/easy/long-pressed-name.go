c76498ebf68d0dcfafc2395c9532039c0f0c8b6b
Son0-0 <jsmith032211@gmail.com>
2023-05-29 15:20:31 +0900

    solve: leetcode 23/05/28

--- leetcode/easy/long-pressed-name.go
+++ leetcode/easy/long-pressed-name.go
@@ -1,5 +1,26 @@
 package main
 
+import "fmt"
+
+func isLongPressedName(name string, typed string) bool {
+	n, t := 0, 0
+
+	for n <= len(name) && t < len(typed) {
+		if n < len(name) && name[n] == typed[t] {
+			n++
+			t++
+		} else if t != 0 && typed[t-1] == typed[t] {
+			t++
+		} else {
+			return false
+		}
+	}
+
+	return n == len(name) && t == len(typed)
+}
+
 func main() {
-  fmt.Println("Hello Leetcode")
+	fmt.Println(isLongPressedName("alex", "aaleex"))
+	fmt.Println(isLongPressedName("alex", "aaleexa"))
+	fmt.Println(isLongPressedName("saeed", "ssaaedd"))
 }
