a8f8507f57f3005b2d722b0cfb1fb8c54dfc1d69
Son0-0 <jsmith032211@gmail.com>
2023-05-31 20:24:29 +0900

    solve: leetcode 23/05/30

--- leetcode/easy/lexicographically-smallest-palindrome.go
+++ leetcode/easy/lexicographically-smallest-palindrome.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func makeSmallestPalindrome(s string) string {
+	size := len(s)
+	result := make([]string, size)
+
+	for i := 0; i < size/2; i++ {
+		if s[i] < s[size-i-1] {
+			result[i] = string(s[i])
+			result[size-i-1] = string(s[i])
+		} else {
+			result[i] = string(s[size-i-1])
+			result[size-i-1] = string(s[size-i-1])
+		}
+	}
+
+	if size%2 != 0 {
+		result[size/2] = string(s[size/2])
+	}
+
+	return strings.Join(result, "")
+}
 
 func main() {
-  fmt.Println("Hello Leetcode")
+	fmt.Println(makeSmallestPalindrome("egcfe"))
+	fmt.Println(makeSmallestPalindrome("abcd"))
+	fmt.Println(makeSmallestPalindrome("seven"))
 }
