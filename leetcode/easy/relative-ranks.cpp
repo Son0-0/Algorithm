f2dea5cae70b616d42398a73e83f1ac5c7ddc657
Son0-0 <jsmith032211@gmail.com>
2023-06-04 23:00:35 +0900

    solve: leetcode 23/06/04

---
+++ leetcode/easy/relative-ranks.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+vector<string> findRelativeRanks(vector<int>& score) {
+	vector<string> result(score.size(), "");
+
+	priority_queue<vector<int>> pq;
+
+	for (int i = 0; i < score.size(); i++) {
+		pq.push(vector<int>{score[i], i});
+	}
+
+	for (int i = 1; i <= score.size(); i++) {
+		vector<int> target = pq.top();
+		pq.pop();
+
+		switch (i) {
+		case 1:
+			result[target[1]] = "Gold Medal";
+			break;
+		case 2:
+			result[target[1]] = "Silver Medal";
+			break;
+		case 3:
+			result[target[1]] = "Bronze Medal";
+			break;
+		default:
+			result[target[1]] = to_string(i);
+			break;
+		}
+	}
+
+	return result;
+}
+
+int main(void) {
+	vector<int> nums{5, 4, 3, 2, 1};
+
+	vector<string> result = findRelativeRanks(nums);
+
+	for (int i = 0; i < result.size(); i++) {
+		cout << result[i] << endl;
+	}
+}
