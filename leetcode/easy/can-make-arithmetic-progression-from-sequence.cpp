4eafc019dff1ff11781a5705e63ff33a0a39bfd6
Son0-0 <jsmith032211@gmail.com>
2023-06-06 16:20:06 +0900

    solve: leetcode 23/06/06

---
+++ leetcode/easy/can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool cmp(int a, int b) {
+	return a > b;
+}
+
+bool canMakeArithmeticProgression(vector<int>& arr) {
+	sort(arr.begin(), arr.end());
+
+	int diff = arr[1] - arr[0];
+	int temp = 0;
+
+	for (int i = 2; i < arr.size(); i++) {
+		temp = arr[i] - arr[i - 1];
+
+		if (diff != temp) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+int main(void) {
+	vector<int> arr{3, 5, 1};
+
+	cout << canMakeArithmeticProgression(arr) << endl;
+
+	arr = { 1, 2, 4 };
+	
+	cout << canMakeArithmeticProgression(arr) << endl;
+}
