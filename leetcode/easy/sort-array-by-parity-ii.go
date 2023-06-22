7dda06e9dc45f244a5def085e4507c990ac4b0ae
Son0-0 <jsmith032211@gmail.com>
2023-06-22 23:30:23 +0900

    solve: leetcode 23/06/22

--- leetcode/easy/sort-array-by-parity-ii.go
+++ leetcode/easy/sort-array-by-parity-ii.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+func sortArrayByParityII(nums []int) []int {
+	result := make([]int, len(nums))
+	i, j := 0, 1
+
+	for _, num := range nums {
+		if num%2 == 0 {
+			result[i] = num
+			i += 2
+		} else {
+			result[j] = num
+			j += 2
+		}
+	}
+
+	return result
+}
+
 func main() {
-    fmt.Println("Hello Leetcode")
+	fmt.Println(sortArrayByParityII([]int{4, 2, 5, 7}))
+	fmt.Println(sortArrayByParityII([]int{2, 3}))
 }
