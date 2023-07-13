d51a592061c3a7496d4c064e35c04481ae9ff535
Son0-0 <jsmith032211@gmail.com>
2023-07-13 21:29:32 +0900

    solve: leetcode 23/07/13

---
+++ leetcode/easy/number-of-good-pairs.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int numIdenticalPairs(vector<int> &nums) {
+  int cnt = 0;
+
+  for (int i = 0; i < nums.size(); i++) {
+    for (int j = i + 1; j < nums.size(); j++) {
+      if (nums[i] == nums[j]) {
+        cnt++;
+      }
+    }
+  }
+
+  return cnt;
+}
+
+int main() {
+  vector<int> nums{1, 2, 3, 1, 1, 3};
+  int result = numIdenticalPairs(nums);
+
+  cout << result;
+}
